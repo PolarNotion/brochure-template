e76ca2bace5cceb35b5bfef84124c71e9a934ead
John Rae <john.rae23@gmail.com>
2016-05-24 15:38:13 -0400

    added bootstrap js import

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 import $ from 'jquery';
 import _ from 'underscore';
 import moment from 'moment';
+import '../node_modules/bootstrap-sass/assets/javascripts/bootstrap.min.js';
 
 console.log('Hello, World');
