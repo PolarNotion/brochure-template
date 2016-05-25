d40a7fbbd63dfbc44dbb3133845fd7cfb078f52e
John Rae <john.rae23@gmail.com>
2016-05-25 11:47:30 -0400

    doin it old school. if we need any other plugins besides bootstrap requiring a global jQuery, it will have to be in the html anyways

--- js/main.js
+++ js/main.js
@@ -1,8 +1,5 @@
-import $ from 'jquery';
-window.$ = window.jQuery = $;
 import _ from 'underscore';
 import moment from 'moment';
-import Bootstrap from '../node_modules/bootstrap-sass/assets/javascripts/bootstrap.min.js';
-Bootstrap.$ = $;
+import '../node_modules/bootstrap-sass/assets/javascripts/bootstrap.min.js';
 
 console.log('Hello, World');
