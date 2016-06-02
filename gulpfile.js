0dafc47988b4a1b59f168afeffbb132ed77cd3e2
John Rae <john.rae23@gmail.com>
2016-06-02 16:33:28 -0400

    changed filestructure to work with app and dist folder, everything gets piped to dist now. added image compression gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -17,6 +17,8 @@ var jscs        = require('gulp-jscs');
 var htmlrender  = require('gulp-htmlrender');
 var watch       = require('gulp-watch'); // A Better File Watcher
 var del         = require('del');
+var imagemin    = require('gulp-imagemin');
+var cache       = require('gulp-cache');
 // Set up Foundation
 var path        = require('path');
 var config = {
@@ -34,69 +36,75 @@ var browserifyError = function(err) {
   this.emit('end');
 }
 
-
 gulp.task('sass', function () {
-  gulp.src('./sass/main.scss')
+  gulp.src('app/sass/main.scss')
     .pipe( notifyError() )
     .pipe(sourcemaps.init({ loadMaps: true }))
     .pipe(sass({
       includePaths: [fontAwesome.scssPath, config.bootstrapDir + '/assets/stylesheets']
     }))
     .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest('./app/css'));
+    .pipe(gulp.dest('./dist/css'));
+});
+
+gulp.task('compress', function(){
+  return gulp.src('app/assets/images/**/*.+(png|jpg|jpeg|gif|svg)')
+  // Caching images that ran through imagemin
+  .pipe(imagemin())
+  .pipe(gulp.dest('dist/images'))
 });
 
 gulp.task('fonts', function() {
   gulp.src(fontAwesome.fonts)
     .pipe( notifyError() )
-    .pipe(gulp.dest('./app/fonts'));
+    .pipe(gulp.dest('./dist/fonts'));
 });
 
 gulp.task('normalize', function() {
   gulp.src(require.resolve('normalize.css'))
     .pipe( notifyError() )
-    .pipe(gulp.dest('./app/css'));
+    .pipe(gulp.dest('./dist/css'));
 });
 
 gulp.task('browserify', function() {
-  return browserify('./js/main.js', {debug: true})
+  return browserify('./app/js/main.js', {debug: true})
     .transform(babel)
     .bundle()
     .on('error', browserifyError)
-    .pipe(source('./main.js'))
+    .pipe(source('./app/main.js'))
     .pipe(buffer())
     .pipe(sourcemaps.init({loadMaps: true}))
     // .pipe(uglify())
     .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest('./app/js'));
+    .pipe(gulp.dest('./dist/js'));
 });
 
-gulp.task('clean:app', function(){
-  return del.sync('app/*.html');
+gulp.task('clean:dist', function(){
+  return del.sync('dist/*.html');
 });
 
 gulp.task('render', function() {
-  return gulp.src('layout/*.html', {read: false})
+  return gulp.src('app/layout/*.html', {read: false})
     .pipe(htmlrender.render())
-    .pipe(gulp.dest('app'))
+    .pipe(gulp.dest('dist'))
 });
 
 // Janky - quick fix to write spec file
 gulp.task('browserify-test', function() {
-  return browserify('./js/tests.js', {debug: true})
+  return browserify('./app/js/tests.js', {debug: true})
     .transform(babel)
     .bundle()
     .on('error', browserifyError)
     .pipe(source('./tests.js'))
     .pipe(buffer())
     .pipe(sourcemaps.init({loadMaps: true}))
-    // .pipe(uglify())
+    .pipe(uglify())
     .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest('./spec/'));
 });
 
 gulp.task('style:js', function() {
-  return gulp.src('./js/**/*.js')
+  return gulp.src('./app/js/**/*.js')
     .pipe(notifyError())
     .pipe(jscs())
     .pipe(jscs.reporter())
@@ -104,7 +112,7 @@ gulp.task('style:js', function() {
 });
 
 gulp.task('hint:js', function() {
-  return gulp.src('./js/**/*.js')
+  return gulp.src('./app/js/**/*.js')
     .pipe(notifyError())
     .pipe(jshint({
       esnext: true, eqeqeq: true,
@@ -117,31 +125,31 @@ gulp.task('hint:js', function() {
 gulp.task('lint', ['style:js', 'hint:js']);
 
 gulp.task('watch', function() {
-  watch(['./layout/**/*.html'], function () {
-    gulp.start('clean:app');
+  watch(['./app/layout/**/*.html'], function () {
+    gulp.start('clean:dist');
     gulp.start('render');
   });
-  watch('./sass/**/*.scss', function () {  
+  watch('./app/sass/**/*.scss', function () {  
     gulp.start('sass'); 
   });
-  watch(['./js/**/*.js', './package.json'], function () {
+  watch(['./app/js/**/*.js', './package.json'], function () {
     gulp.start('browserify');
     gulp.start('browserify-test');
   });
-  watch('./js/**/*.js', function () {
+  watch('./app/js/**/*.js', function () {
     gulp.start('hint:js');
     gulp.start('style:js');
   });
 });
 
 gulp.task('server', ['default'], function () {
-  return gulp.src('app')
+  return gulp.src('dist')
     .pipe(server({
       livereload: true
     }));
 });
 
-gulp.task('default', ['clean:app',
+gulp.task('default', ['clean:dist',
                       'render',
                       'sass',
                       'fonts',
